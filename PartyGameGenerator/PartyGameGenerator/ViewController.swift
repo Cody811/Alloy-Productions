24f8fec4594c09289c2bf4019e55bb8bcccb529f
PlayOnwards <cody.edgington@gmail.com>
2016-10-17 10:04:01 -0700

    Git init

---
+++ PartyGameGenerator/PartyGameGenerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PartyGameGenerator
+//
+//  Created by Cody Edgington on 10/17/16.
+//  Copyright © 2016 Cody Edgington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
